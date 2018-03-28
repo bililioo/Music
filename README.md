0a10b3c12335aa33911068de3cb7e7adc938d228
BunChan <bunchan16@yahoo.com>
2018-03-28 11:04:48 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Music
+> *这是一个使用Python实现的音乐下载网站。*
+
+### 环境要求
+* `Python3`
+* `MySQL`
+
+Python需要安装包`jinja2`、`aiohttp`、`aiohttp_cors`。
+
+使用以下命令可以安装包：
+
+```bash
+$ pip3 install jinja2
+$ pip3 install aiohttp
+$ pip3 install aiohtt_cors
+```
+### 运行项目
+
+- 创建本地数据库，在路径下执行以下命令：
+
+    ```$ mysql -u root -p < music.sql ```
+
+- 修改`config_override.py`文件中的配置，可参考`config_default.py`进行修改。
+
+- 执行`music_source.py`将歌曲信息插入`music`表中。
+- 执行`app.py`
+
+
--- music_source.py
+++ music_source.py
@@ -4,8 +4,9 @@
 import os, orm, asyncio
 from datetime import datetime
 from models import Song
+from config import configs
 
-pwd = "/Users/chenbin/Music/虾米音乐/"
+pwd = configs.songs['path']
 print('      Size     Last Modified  Name')
 
 for f in os.listdir(pwd):
@@ -16,20 +17,20 @@ for f in os.listdir(pwd):
             fsize = float(fsize) / 1024 / 1024
             print('%.2fM  %s  %s' % (fsize, mtime, f))
 
-# async def insert_data(loop):
-#     await orm.create_pool(loop, host='127.0.0.1', user='root', password='990978664', db='music')
+async def insert_data(loop):
+    await orm.create_pool(loop, host='127.0.0.1', user='root', password='990978664', db='music')
 
-#     for f in os.listdir(pwd):
-#         abspath = pwd + f
-#         fsize = os.path.getsize(abspath)
-#         mtime = datetime.fromtimestamp(os.path.getmtime(abspath)).strftime('%Y-%m-%d %H:%M')
-#         if fsize > 500:
-#             fsize = float(fsize) / 1024 / 1024
-#             print('%.2fM  %s  %s' % (fsize, mtime, f))
-#             song = Song(path=abspath, title=f, date=mtime, size=fsize)
-#             await song.save()
+    for f in os.listdir(pwd):
+        abspath = pwd + f
+        fsize = os.path.getsize(abspath)
+        mtime = datetime.fromtimestamp(os.path.getmtime(abspath)).strftime('%Y-%m-%d %H:%M')
+        if fsize > 500:
+            fsize = float(fsize) / 1024 / 1024
+            print('%.2fM  %s  %s' % (fsize, mtime, f))
+            song = Song(path=abspath, title=f, date=mtime, size=fsize)
+            await song.save()
         
 
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(insert_data(loop))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(insert_data(loop))
                 
